ac27c9407109a22550dfcd2cbf0c6e91b705e7c6
Fabien Villedieu <fabien.villedieu.pro@gmail.com>
2024-11-28 11:17:27 +0100

    #8 Test employé

--- GestionEmployes/GmTP.cpp
+++ GestionEmployes/GmTP.cpp
@@ -6,6 +6,18 @@ int main(){
 
 	cout << "BIENVENUE DANS GESTION DES EMPLOYES\n";
 
+	employe E1;
+	strcpy_s(E1.nom, "Martinez");
+	strcpy_s(E1.prenom, "David");
 
+	E1.date_embauche.jour = 5;
+	E1.date_embauche.mois = 7;
+	E1.date_embauche.annee = 2005;
+
+	E1.date_fonction.jour = 10;
+	E1.date_fonction.mois = 8;
+	E1.date_fonction.annee = 2005;
+
+	cout << "Employe 1 : " << E1.prenom << " " << E1.nom << "\nDate d'embauche : " << E1.date_embauche.jour << "/" << E1.date_embauche.mois << "/" << E1.date_embauche.annee << "\nDate de prise de fonction : " << E1.date_fonction.jour << "/" << E1.date_fonction.mois << "/" << E1.date_fonction.annee;
 	return 0;
 }
